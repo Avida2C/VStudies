4d62ec6ce904d24e9e23d649a94369631d7e456f
Nadine <n4dinevid4l@hotmail.com>
2023-08-13 11:39:35 +0200

    getNumberOfLines - Project

--- Javascript/project.js
+++ Javascript/project.js
@@ -24,7 +24,7 @@ const deposit = () => {
         //Converting string to number
         const depositNumberAmount = parseFloat(depositAmount);
 
-        //If the user inputs a string or a number less or equal to 0, it will tell the user that it is an invalid amount
+        //If the number isNaN (is Not a Number), check whether the input is a string... less than or equal to 0
         if (isNaN(depositNumberAmount) || depositNumberAmount <= 0) {
             console.log("Invalid deposit amount, try again.")
         } else {
@@ -33,8 +33,44 @@ const deposit = () => {
     }
 }
 
-//calling the function deposit
-//this function will display the valid amount provided by the user and concludes the loop
-const depositAmount = deposit();
-console.log(depositAmount);
 
+//The following is a function work similarly to: function deposit () {}
+const getNumberOfLines = () => {
+    //The function will keep looping until the user provides a valid amount
+    while (true) {
+
+        //Asking the user to input amount
+        const lines = promt("Enter the number of lines to bet on (1-3): ");
+
+        //Converting string to number
+        const numberOfLines = parseFloat(lines);
+
+        //If the number isNaN (is Not a Number), check whether the input is a string... less than or equal to 0... greater than 3
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.")
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+/* Calling the function: deposit
+This function will:
+1. Ask the user to input a number
+2. Convert the string to number
+3. Determine whether the input is a valid number (invalid: string, smaller than 0)
+4. If the number is not valid, the function will keep asking the user to enter a valid number
+5. The loop ends once the user inputs a valid number.
+*/
+
+const depositAmount = deposit();
+
+/* Calling the function: getNumberOfLines
+This function will:
+1. Ask the user to input a number
+2. Convert the string to number
+3. Determine whether the input is a valid number (invalid: string, smaller than 0, or greater than 3)
+4. If the number is not valid, the function will keep asking the user to enter a valid number
+5. The loop ends once the user inputs a valid number.
+*/
+const lines = getNumberOfLines();

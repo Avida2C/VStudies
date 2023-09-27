85536b94b9171c2023dc66358bedcb5b4d412678
Nadine <n4dinevid4l@hotmail.com>
2023-09-27 21:04:31 +0200

    login

---
+++ admin.php
@@ -0,0 +1,24 @@
+<?php
+require "functions.php";
+
+check_login();
+?>
+
+<body class="bodyStyle">
+    <?php require_once 'include/header.php';
+    require_once 'include/navbar.php';?>
+
+    <div class="wrapper">
+        <div id=" ">
+            <h3>Admin dashboard</h3>
+            <p>Hi <?php echo $_SESSION['USER']['username']?>, Welcome!</p>
+
+        </div>
+    </div>
+
+    <?php 
+    require_once "include/footer.php";
+    require "include/scripts.php"; ?>
+</body>
+
+</html>
--- include/navbar.php
+++ include/navbar.php
@@ -4,7 +4,7 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse px-5" style="justify-content: center;" id="navbarNavDropdown">
-        <a class="navbar-brand" style="margin-right:15em;" href="index.php">VStudies</a>
+        <a class="navbar-brand" style="margin-right:10em;" href="index.php">VStudies</a>
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="javascriptNotes.php">Javascript</a>
@@ -29,6 +29,9 @@
                 <a class="nav-link active" aria-current="page" href="signup.php">Signup</a>
             </li>
             <?php else: ?>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="admin.php">Admin</a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
             </li>

496305e5a6e1b7a43266f7690a08dcebd04a96b8
Nadine <n4dinevid4l@hotmail.com>
2023-10-01 13:03:53 +0200

    phpMailer Installed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '874467999976ae06cc36536b0b12b28570da48e5',
+        'reference' => 'a112c75f66db14ea54f184793039fbbec667992b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '874467999976ae06cc36536b0b12b28570da48e5',
+            'reference' => 'a112c75f66db14ea54f184793039fbbec667992b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

920367a96301fd2fb38606df1402eeead2b40b98
Nadine Vidal <n4dinevid4l@hotmail.com>
2023-10-19 10:39:13 +0200

    Fix on Navbar, W.I.P - still requires Proper fixing and double checking.
    The website still requires responsive settings.

--- include/navbar.php
+++ include/navbar.php
@@ -1,38 +1,47 @@
-<nav class="navbar navbar-expand-lg bg-light p-2 shadow-sm">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse px-5" style="justify-content: center;" id="navbarNavDropdown">
-        <a class="navbar-brand" style="margin-right:10em;" href="index.php">VStudies</a>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="javascriptNotes.php">Javascript</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="regEx.php">RegEx</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="phpNotes.php">PHP</a>
-            </li>
-            <a class="nav-link active" aria-current="page" href="apiNotes.php">API</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="referenceList.php">References</a>
-            </li>
-            <?php if(empty($_SESSION['USER'])) :
-                ?>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="login.php">My Account</a>
-            </li>
-            <?php else: ?>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="admin.php">Admin</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
-            </li>
-            <?php endif; ?>
-        </ul>
-    </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-light shadow-sm">
+    <div class="container-fluid col-lg-8">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">VStudies</a>
+
+        <div class="offcanvas offcanvas-start" style="justify-content: center;" tabindex="-1" id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">VStudies</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="javascriptNotes.php">Javascript</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="regEx.php">RegEx</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="phpNotes.php">PHP</a>
+                    </li>
+                    <a class="nav-link active" aria-current="page" href="apiNotes.php">API</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="referenceList.php">References</a>
+                    </li>
+                    <?php if(empty($_SESSION['USER'])) :
+                        ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="login.php">My Account</a>
+                    </li>
+                    <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="admin.php">Admin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
+                    </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
 </nav>
